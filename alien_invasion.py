0f0335b9955df128759f0298a084be2ffe371963
rohit <rohitdevops.roy@gmail.com>
2021-11-09 23:30:17 +0530

    craeting alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -47,17 +47,33 @@ class AlienInvasion:
         #create an alien and calculate the number of aliens in a row 
         #spacing between each alien is equal to one alien width 
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
         
-        #create the first row of aliens
-        #loop from 0 to number of aliens which would fit in a single row  
-        for alien_number in range(number_aliens_x):
-            #create an alien and place it in a row 
+        #determine the number of rows of aliens that fit on the screen 
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+        
+        #create full fleet of aliens 
+        #outer loop calculates the number of rows we require 
+        for row_number in range(number_rows):
+            #create the first row of aliens
+            #loop from 0 to number of aliens which would fit in a single row  
+            #the inner loop calculates the number of aliens we require is a single row 
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+            
+    def _create_aliens(self, alien_number, row_number):
+        #create an alien and place it in a row 
             alien = Alien(self)
+            alien_width, alien_height = alien.rect.size
             alien.x = alien_width + 2 * alien_width * alien_number
+            #alien's x coordinate 
             alien.rect.x = alien.x
+            #alien's y coordinate
+            alien.rect.y = alien.rect.height + 2* alien.rect.height * row_number
             self.aliens.add(alien)
         
     def _check_events(self):
